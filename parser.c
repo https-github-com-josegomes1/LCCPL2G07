c8a79ff58764b38b061f359786e0e8a6ff7351a7
Zé Pedro Gomes <78722983+josegomes1@users.noreply.github.com>
2021-03-21 15:19:49 +0000

    Create parser.c

---
+++ parser.c
@@ -0,0 +1,11 @@
+/**
+ * @file Ficheiro que contém as funcionalidades relativas ao parser do projeto
+ * 
+ * 
+ */
+#include <assert.h>
+#include "parser.h"
+#include "stack.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
