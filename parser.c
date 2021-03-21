0b318bd361af0e0107c9c7b4a675b6051885653e
Zé Pedro Gomes <78722983+josegomes1@users.noreply.github.com>
2021-03-21 15:20:56 +0000

    Update parser.c

--- parser.c
+++ parser.c
@@ -9,3 +9,27 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * \brief Esta função divide a linha em tokens e interpreta os mesmos
+ * 
+ * @param line A linha lida do stdin 
+ */
+
+void parse(char * line) {
+
+    char *delimits = " \t\n";
+    char *sobra;
+
+    long operando1;
+    long operando2;
+
+    for (char * token = strtok(line, delimits); token != NULL; token = strtok(NULL, delimits)) {
+
+        operando1 = strtol(token, &sobra, 10);
+
+        if (strlen(sobra) == 0) {
+
+            push(operando1);
+
+        }
