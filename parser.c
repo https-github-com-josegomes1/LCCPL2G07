ffae166ad38aa9da6d55b42a89c0439a9f00cc06
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2021-03-27 18:26:36 +0000

    Documentação

--- parser.c
+++ parser.c
@@ -1,14 +1,17 @@
 /**
  * @file Ficheiro que contém as funcionalidades relativas ao parser do projeto
- * 
- * 
  */
+
 #include <assert.h>
 #include "parser.h"
 #include "stack.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * \brief Esta função faz a operação de exponenciação 
+ */
 
 long potencia (int, int);
 
