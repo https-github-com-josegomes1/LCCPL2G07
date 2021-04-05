262a4ed027cfadfa8a04920d0dd98f58ac7ee609
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2021-04-05 15:20:56 +0100

    Documentação

--- parser.h
+++ parser.h
@@ -1,5 +1,3 @@
-/**
- * @file Arquivo que contém as funções do parser.c que precisam ser passadas entre arquivos
- */
+/** @file Arquivo que contém as funções do parser.c que precisam ser passadas entre arquivos */
 
 void parse (char * line);
