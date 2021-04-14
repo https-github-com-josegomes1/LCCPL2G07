d412c91c0a3f4fe49efae827a906c289b0706ce8
EduDCS <77997232+EduardoDCS2002@users.noreply.github.com>
2021-04-14 13:52:38 +0100

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,3 +1,8 @@
 /** @file Arquivo que contém as funções do parser.c que precisam ser passadas entre arquivos */
 
-void parse (char * line);
+#ifndef PARSE_H
+#define PARSE_H
+
+void parse(STACK *s, char*);
+
+#endif
