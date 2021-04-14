6ce7ca96f4c70130fcefa355b19ad2d8f5896d74
EduDCS <77997232+EduardoDCS2002@users.noreply.github.com>
2021-04-14 13:53:05 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,8 +1,64 @@
 /** @file Arquivo que contém as funções do stack.c que precisam ser passadas entre arquivos */
 
-int IsStackFull();
-int IsStackEmpty();
-void push(long);
-long pop();
-void pop_double(long *, long *);
-void PrintStack ();
+#ifndef STACK_H
+#define STACK_H
+
+typedef enum {INT, DOUBLE, CHAR, STRING, ARRAY} TYPE;
+
+typedef struct data {
+   TYPE type;
+   union {
+      int INT;
+      double DOUBLE;
+      char CHAR;
+      char *STRING;
+      struct {
+         struct data *elems;
+         int size;
+      } ARRAY;
+   };
+
+} DATA;
+
+typedef struct stack {
+   DATA *stack;
+   DATA *variables;
+   int size;
+   int pos;
+} STACK;
+
+void create_stack (STACK *s);
+
+void push(STACK *s, DATA elem);
+
+DATA pop(STACK *s);
+
+void swap_data(STACK * s);
+
+void rotate_top(STACK * s);
+
+void copy_nth_element(STACK *s);
+
+DATA top (STACK *s);
+
+// Converts
+
+void convert_to_char(STACK * s);
+void convert_to_int(STACK * s);
+void convert_to_string(STACK * s);
+void convert_to_float(STACK * s);
+
+// ...
+
+void default_variables(STACK *s);
+
+#define STACK_OPERATION_PROTO(_type, _name) \
+   void push_##_name(STACK *s, _type);      \
+   _type pop_##_name(STACK *s);             
+
+STACK_OPERATION_PROTO(int, INT)
+STACK_OPERATION_PROTO(double, DOUBLE)
+STACK_OPERATION_PROTO(char, CHAR)
+STACK_OPERATION_PROTO(char*, STRING)
+
+#endif
