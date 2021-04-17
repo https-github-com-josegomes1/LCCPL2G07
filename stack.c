acbd4692038699ceb257585c05d0a2f7640b665a
zeisalone <79376027+zeisalone@users.noreply.github.com>
2021-04-17 20:33:52 +0100

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,170 +1,52 @@
-/** @file Ficheiro que contém as funcionalidades relativas a stack do projeto */
+/** @file Ficheiro que contém as funcionalidades relativas a stack */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "stack.h"
 
-/**
- * \brief Inicializa os valores da stack
- */
-
-void create_stack (STACK *s) {
-    s->pos = -1;
-    s->size = 32;
+STACK* create_stack () {
+    STACK *s = malloc(sizeof(STACK));
+    s->pos = -1; // -1 -> posição inicial: representa que a stack que está vazia
+    s->size = 32; // Definimos um tamanho inicial para a stack e aumentamos conforme seja preciso
     s->stack = malloc(sizeof(DATA) * s->size);
-    s->variables = malloc(sizeof(DATA) * 26);
+    s->variables = malloc(sizeof(DATA) * 26); // 26 -> Variáveis de A até Z
+    return s;
 }
 
-/**
- * \brief adiciona um elemento ao topo da stack
- */
-
 void push(STACK *s, DATA elem) {
-    if (s->pos == s->size - 1) {
-        s->size += 32;
+    if (s->pos == s->size - 1) { // Se a stack estiver cheia, aumentamos ela
+        s->size += 32;           // assim enquanto houver espaço na nossa memória física, há espaço na stack
         s->stack = realloc(s->stack, sizeof(DATA) * s->size);
     }
     s->stack[++s->pos] = elem;
 }
 
-/**
- * \brief retira um elemento do topo da stack
- */
-
 DATA pop(STACK *s) {
     return s->stack[s->pos--];
 }
-
-/**
- * \brief devolve o elemento que está no topo da stack
- */
 
 DATA top (STACK *s) {
     return s->stack[s->pos];
 }
 
-/**
- * \brief troca os dois elementos do top da stack
- */
-
-void swap_data (STACK *s) {
-    DATA data1 = pop(s);
-    DATA data2 = pop(s);
-    push(s, data1);
-    push(s, data2);
+void push_unary_operation(STACK *s, double val) {                           // val tipo -> push tipo
+    if (s->stack[s->pos + 1].type == DOUBLE) {push_DOUBLE(s, val); return;} // DOUBLE   -> DOUBLE
+    if (s->stack[s->pos + 1].type == INT) {push_INT(s, val); return;}       // DOUBLE   -> INT
+    push_CHAR(s, val);                                                      // DOUBLE   -> CHAR
 }
 
-/**
- * \brief roda os três elementos do topo da stack
- */
-
-void rotate_top(STACK *s) {
-    DATA data1 = pop(s);
-    DATA data2 = pop(s);
-    DATA data3 = pop(s);
-    push(s, data2);
-    push(s, data1);
-    push(s, data3);
+void push_binary_operation(STACK *s, double val) {
+    if (s->stack[s->pos + 1].type == DOUBLE || s->stack[s->pos + 2].type == DOUBLE) { // val tipo -> push tipo
+        push_DOUBLE(s, val); return;}                                                 // DOUBLE   -> DOUBLE
+    push_INT(s, val);                                                                 // DOUBLE   -> INT
 }
 
-/**
- * \brief  Copia n-ésimo elemento para o topo da stack
- * 0 é o topo da stack neste caso
- */
-
-void copy_nth_element(STACK *s) {
-    int n = pop_INT(s);
-    push(s, s->stack[s->pos - n]);
-}
-
-/**
- * \brief converte o topo da stack em char
- */
-
-void convert_to_char(STACK * s) {
-    DATA elem = pop(s);
-    switch(elem.type) {
-        case INT: push_CHAR(s, elem.INT); break;
-        case DOUBLE: push_CHAR(s, elem.DOUBLE); break;
-        case STRING: push_CHAR(s, *elem.STRING); break;
-        case CHAR:
-        case ARRAY:
-            break;
-    }
-}
-
-/**
- * \brief converte o topo da stack em int
- */
-
-void convert_to_int(STACK * s) {
-    DATA elem = pop(s);
-    switch(elem.type) {
-        case CHAR: push_INT(s, elem.CHAR); return;
-        case DOUBLE: push_INT(s, elem.DOUBLE); return;
-        case STRING:
-        case ARRAY:
-        case INT:
-            break;
-    }
-    char * sobra;
-    int INT = strtol(elem.STRING, &sobra, 10);
-    push_INT(s, INT);
-}
-
-/**
- * \brief converte o topo da stack em float
- */
-
-void convert_to_float(STACK * s) {
-    char * sobra;
-    DATA elem = pop(s);
-    double elem_DOUBLE = elem.DOUBLE;
-    switch(elem.type) {
-        case CHAR: push_DOUBLE(s, elem.CHAR); return;
-        case INT: push_DOUBLE(s, elem.INT); return;
-        case STRING: elem_DOUBLE = strtod(elem.STRING, &sobra); break;
-        case DOUBLE: break;
-        case ARRAY: return;
-    }
-    push_DOUBLE(s, elem_DOUBLE);
-}
-
-/**
- * \brief converte o topo da stack em string
- */
-
-void convert_to_string(STACK * s) {
-    DATA data = pop(s);
-    char * string = malloc(sizeof(char) * 20);
-    switch(data.type) {
-        case CHAR: string[0] = data.CHAR; break;
-        case INT: snprintf(string, 20, "%d", data.INT); break;
-        case DOUBLE: snprintf(string, 20, "%g", data.DOUBLE); break;
-        case STRING:
-        case ARRAY:
-            break;
-    }
-    push_STRING(s, string);
-}
-
-/**
- * \brief define os valores por omissão das variáveis
- */
-
-void default_variables(STACK *s) {
-    s->variables[0].INT = 10; s->variables[0].type = INT;              // A
-    s->variables[1].INT = 11; s->variables[1].type = INT;              // B
-    s->variables[2].INT = 12; s->variables[2].type = INT;              // C
-    s->variables[3].INT = 13; s->variables[3].type = INT;              // D
-    s->variables[4].INT = 14; s->variables[4].type = INT;              // E
-    s->variables[5].INT = 15; s->variables[5].type = INT;              // F
-    s->variables[13].STRING = "\n"; s->variables[13].type = STRING;    // N
-    s->variables[18].STRING = " "; s->variables[18].type = STRING;     // S
-    s->variables[23].INT = 0; s->variables[23].type = INT;             // X
-    s->variables[24].INT = 1; s->variables[24].type = INT;             // Y
-    s->variables[25].INT = 2; s->variables[25].type = INT;             // Z
+double pop_operand(STACK *s) { // O double consegue armazenar os três tipos, sem trocar o valor do elemento
+    DATA elem = pop(s);                          // pop tipo -> retorna 
+    if (elem.type == INT) return elem.INT;       // INT      -> DOUBLE
+    if (elem.type == DOUBLE) return elem.DOUBLE; // DOUBLE   -> DOUBLE
+    return elem.CHAR;                            // CHAR     -> DOUBLE
 }
 
 /**
