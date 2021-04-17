a4238006f1a3ffe62b4f4cc9c7c99b91d5cfd0c4
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2021-04-17 20:31:34 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+/** @file Ficheiro principal do programa */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -5,21 +7,23 @@
 #include "stack.h"
 #include "parser.h"
 #include "IO.h"
+#include "variables.h"
+
+/** \brief função principal do programa 
+ * Inicializa as variáveis, recebe o input do usuário e faz parse da string obtida
+ */
 
 int main () {
 
-    STACK s;
+    STACK *s = create_stack();
+    default_variables_value(s);
+    default_variables_type(s);
+
     char line[10240];
-
-    create_stack(&s);
-    default_variables(&s);
-
     assert(fgets(line, 10240, stdin) != NULL);
+    parse(s, line);
 
-    parse(&s, line);
-
-    print_stack(&s);
-
+    print_stack(s);
     return 0; 
 
 }
