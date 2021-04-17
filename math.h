760c095417d96aa408e04f038090fd9db0f4a9f1
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2021-04-17 20:34:00 +0100

    Update math.h

--- math.h
+++ math.h
@@ -1,13 +1,42 @@
-/** @file Arquivo que contém as funções do math.c que precisam ser passadas entre arquivos */
+/** @file Arquivo que contém as funções do math.c */
 
-#ifndef math_H
-#define math_H
+#ifndef MATH_H
+#define MATH_H
 
-void push_unary_operation(STACK *s, double val);
-void push_binary_operation(STACK *s, double val);
-double pop_operand(STACK *s);
+/** \brief efetua uma operação de soma e da push do resultado para stack */
+void sum(STACK *s);
 
-void if_then_else (STACK *s);
-void get_logic_state(STACK *s, char token);
+/** \brief efetua uma operação de subtração e da push do resultado para stack */
+void subtraction(STACK *s);
+
+/** \brief efetua uma operação de multiplicação e da push do resultado para stack */
+void multiplication(STACK *s);
+
+/** \brief efetua uma operação de divisão e da push do resultado para stack */
+void division(STACK *s);
+
+/** \brief efetua uma operação de potência e da push do resultado para stack */
+void power(STACK *s);
+
+/** \brief efetua uma operação de módulo (resto da divisão) e da push do resultado para stack */
+void module(STACK *s);
+
+/** \brief efetua uma operação de "ou" (em relação aos bits) e da push do resultado para stack */
+void or_bitwise(STACK *s);
+
+/** \brief efetua uma operação de "e" (em relação aos bits) e da push do resultado para stack */
+void and_bitwise(STACK *s);
+
+/** \brief efetua uma operação de "ou exclusivo" (em relação aos bits) e da push do resultado para stack */
+void xor_bitwise(STACK *s);
+
+/** \brief efetua uma operação de "negar" (em relação aos bits) e da push do resultado para stack */
+void not_bitwise(STACK *s);
+
+/** \brief efetua uma operação de incremento e da push do resultado para stack */
+void increment(STACK *s);
+
+/** \brief efetua uma operação de decremento e da push do resultado para stack */
+void decrement(STACK *s);
 
 #endif
