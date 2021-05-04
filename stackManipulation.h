076fcb41459e7fe5ef534562d3b45d6e587d48b4
LeonardoLordelloFontes <77800309+LeonardoLordelloFontes@users.noreply.github.com>
2021-05-04 14:12:24 +0100

    Update stackManipulation.h

--- stackManipulation.h
+++ stackManipulation.h
@@ -5,28 +5,28 @@
 
 #include "stack.h"
 
-/* \brief duplica o topo da stack*/
+/** \brief duplica o topo da stack*/
 void duplicate_top(STACK *s);
 
-/* \brief troca os 2 elementos do topo da stack*/
+/** \brief troca os 2 elementos do topo da stack*/
 void swap_top(STACK *s);
 
-/* \brief roda os 3 elementos do topo da stack*/
+/** \brief roda os 3 elementos do topo da stack*/
 void rotate_top(STACK *s);
 
-/* \brief copia o n-ésimo elemento para o topo da stack*/
+/** \brief copia o n-ésimo elemento para o topo da stack*/
 void copy_nth_element(STACK *s);
 
-/* \brief Converte o topo da stack para um inteiro */
+/** \brief Converte o topo da stack para um inteiro */
 void convert_stack_to_int(STACK *s);
 
-/* \brief Converte o topo da stack para um float */
+/** \brief Converte o topo da stack para um float */
 void convert_stack_to_float(STACK *s);
 
-/* \brief Converte o topo da stack para um char */
+/** \brief Converte o topo da stack para um char */
 void convert_stack_to_char(STACK *s);
 
-/* \brief Converte o topo da stack para uma string */
+/** \brief Converte o topo da stack para uma string */
 void convert_stack_to_string(STACK *s);
 
 #endif
