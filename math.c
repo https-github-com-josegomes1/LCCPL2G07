b19e7f313f49f9c6b39f833eb6b88696e00f1024
zeisalone <79376027+zeisalone@users.noreply.github.com>
2021-05-04 11:17:22 +0100

    Update math.c

--- math.c
+++ math.c
@@ -3,40 +3,40 @@
 #include <math.h>
 #include "stack.h"
 
-void sum(STACK *s) {                  // 1 2 3 <- STACK -> VALORES <> 3 1.5 2 <- STACK -> VALORES
-    double x = pop_operand(s),        // 1 2   <- pop   -> 3       <> 3 1.5   <- pop   -> 2
-           y = pop_operand(s);        // 1     <- pop   -> 2       <> 3       <- pop   -> 1.5
-    push_binary_operation(s, y + x);  // 1 5   <- push  -> 2+3=5   <> 3 3.5   <- push  -> 1.5+2=3.5
+void sum(STACK *s) {                 
+    long double x = pop_operand(s),        
+                y = pop_operand(s);        
+    push_binary_operation(s, y + x);  
 }
 
-void subtraction(STACK *s) {          // 1 5 3 <- STACK -> VALORES <> 3 2 0.5 <- STACK -> VALORES
-    double x = pop_operand(s),        // 1 5   <- pop   -> 3       <> 3 2     <- pop   -> 0.5
-           y = pop_operand(s);        // 1     <- pop   -> 5       <> 3       <- pop   -> 2
-    push_binary_operation(s, y - x);  // 1 2   <- push  -> 5-3=2   <> 3 1.5   <- push  -> 2-0.5=1.5
+void subtraction(STACK *s) {          
+    long double x = pop_operand(s),        
+                y = pop_operand(s);        
+    push_binary_operation(s, y - x); 
 }
 
-void multiplication(STACK *s) {       // 1 2 3 <- STACK -> VALORES <> 1 2.3 3 <- STACK -> VALORES
-    double x = pop_operand(s),        // 1 2   <- pop   -> 3       <> 1 2.3   <- pop   -> 3 
-           y = pop_operand(s);        // 1     <- pop   -> 2       <> 1       <- pop   -> 2.3
-    push_binary_operation(s, y * x);  // 1 6   <- push  -> 2*3=6   <> 1 6.9   <- push  -> 2.3*3=6.9
+void multiplication(STACK *s) {       
+    long double x = pop_operand(s),       
+                y = pop_operand(s);        
+    push_binary_operation(s, y * x); 
 }
 
-void division(STACK *s) {            // 1 7 3 <- STACK -> VALORES <> 1 7.0 3 <- STACK -> VALORES
-    double x = pop_operand(s),       // 1 7   <- pop   -> 3       <> 1 7.0   <- pop   -> 3
-           y = pop_operand(s);       // 1     <- pop   -> 7       <> 1       <- pop   -> 7.0
-    push_binary_operation(s, y / x); // 1 2   <- push  -> 7/3=2   <> 1 3.5   <- push  -> 7.0/3=3.5
+void division(STACK *s) {            
+    long double x = pop_operand(s),       
+                y = pop_operand(s);       
+    push_binary_operation(s, y / x); 
 }
 
-void power(STACK *s) {                   // 1 2 3 <- STACK -> VALORES <> 1 9 0.5 <- STACK -> VALORES
-    double x = pop_operand(s),           // 1 2   <- pop   -> 3       <> 1 9     <- pop   -> 0.5 
-           y = pop_operand(s);           // 1     <- pop   -> 2       <> 1       <- pop   -> 9
-    push_binary_operation(s, pow(y,x));  // 1 8   <- push  -> 2^3=8   <> 1 3.0   <- push  -> 9^0.5=3.0                    
-}
+void power(STACK *s) {                   
+    long double x = pop_operand(s),          
+                y = pop_operand(s);           
+    push_binary_operation(s, pow(y,x));
+} 
 
-void module(STACK *s) {               // 1 7 3 <- STACK -> VALORES
-    int x = pop_INT(s),               // 1 7   <- pop   -> 3
-        y = pop_INT(s);               // 1     <- pop   -> 7
-    push_binary_operation(s, y % x);  // 1 1   <- push  -> 7 % 3 = 1
+void module(STACK *s) {         
+    int x = pop_INT(s),              
+        y = pop_INT(s);              
+    push_binary_operation(s, y % x); 
 }
 
 void or_bitwise(STACK *s) {          // 1 2 3 <- STACK -> VALORES
