9a88cf074537b778a2d2f469b0d7d45cd20ef594
EduDCS <77997232+EduardoDCS2002@users.noreply.github.com>
2021-05-04 11:20:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,29 +1,31 @@
 /** @file Ficheiro principal do programa */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
+#include "tokenizer.h"
 #include "stack.h"
 #include "parser.h"
 #include "IO.h"
 #include "variables.h"
 
-/** \brief função principal do programa 
- * Inicializa as variáveis, recebe o input do usuário e faz parse da string obtida
- */
-
 int main () {
 
     STACK *s = create_stack();
+    TOKENIZER *tokenizer = NULL;
+
     default_variables_value(s);
     default_variables_type(s);
 
     char line[10240];
     assert(fgets(line, 10240, stdin) != NULL);
-    parse(s, line);
+
+    tokenizer = tokenize(tokenizer, line);
+
+    parse_tokens(s, tokenizer);
 
     print_stack(s);
-    return 0; 
 
+    return 0;
 }
