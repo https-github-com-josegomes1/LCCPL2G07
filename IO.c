84f7bf2e443126165d5c25e6c1c49ab6d6dd334e
EduDCS <77997232+EduardoDCS2002@users.noreply.github.com>
2021-05-04 11:16:25 +0100

    Update IO.c

--- IO.c
+++ IO.c
@@ -13,9 +13,9 @@ double convert_string_to_double(char *string) {
     return DOUBLE;
 }
 
-int convert_string_to_int(char *string) {
+long convert_string_to_int(char *string) {
     char *rest; // variável auxiliar para acompanhar a string na função "strtol"
-    int INT = strtol(string, &rest, 10); // STRING -> INT
+    long INT = strtol(string, &rest, 10); // STRING -> INT
     return INT;
 }
 
@@ -26,14 +26,6 @@ int Is_Double(char *token) {
     return 0;
 }
 
-int Is_array (STACK * s) {
-    return s->stack[s->pos].type == ARRAY || s->stack[s->pos - 1].type == ARRAY;
-}
-
-int Is_String (STACK * s) {
-    return s->stack[s->pos].type == STRING || s->stack[s->pos - 1].type == STRING;
-}
-
 void read_line(STACK * s) {
     char * input = malloc(sizeof(char) * 10240);
     assert(fgets(input, 10240, stdin) != NULL);
@@ -41,19 +33,45 @@ void read_line(STACK * s) {
     push_STRING(s, input);
 }
 
-void print_data(DATA data) {
+void read_all_input(STACK *s) {
+    char *input = malloc(sizeof(char) * 10240);
+    char c;
+    int i = 0;
+    while((c = getchar()) != EOF) {
+        input[i] = c;
+        i++;
+    }
+    input[i+1] = '\0';
+    push_STRING(s, input);
+}
+
+void print_array(STACK *s, DATA a) {
+    for (int i = 0; i < a.array.size; i++) {
+        switch(a.array.elems[i].type) {
+            case ARRAY: print_array(s, a.array.elems[i]); break;
+            case INT: printf("%ld", a.array.elems[i].INT); break;
+            case DOUBLE: printf("%g", a.array.elems[i].DOUBLE); break;
+            case CHAR: printf("%c", a.array.elems[i].CHAR); break;
+            case STRING: printf("%s", a.array.elems[i].STRING); break;
+            default: break;
+        }
+    }
+}
+
+void print_data(STACK *s, DATA data) {
     switch(data.type) {
-        case INT: printf("%d", data.INT); break;
+        case INT: printf("%ld", data.INT); break;
         case DOUBLE: printf("%g", data.DOUBLE); break;
         case CHAR: printf("%c", data.CHAR); break;
         case STRING: printf("%s", data.STRING); break;
-        case ARRAY: break;
+        case BLOCK: printf("%s", data.BLOCK); break;
+        case ARRAY: print_array(s, data); break;
     }
 }
 
 void print_stack(STACK *s) {
     for (int i = 0; i <= s->pos; i++) {
-        print_data(s->stack[i]);
+        print_data(s, s->stack[i]);
     }
     putchar('\n');
 }
