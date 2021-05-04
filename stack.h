3bd02b552647d438fe726898b6d3239fe126f7fd
zeisalone <79376027+zeisalone@users.noreply.github.com>
2021-05-04 11:21:33 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -3,38 +3,51 @@
 #ifndef STACK_H
 #define STACK_H
 
-/** a enumeração dos tipos que os elementos podem ter */
+/** \enum a enumeração dos tipos que os elementos podem ter */
 
 typedef enum {
-   INT,     /**< um inteiro */
-   DOUBLE,  /**< um double */
-   CHAR,    /**< um char */
-   STRING,  /**< uma string */
-   ARRAY    /**< um array */
+   INT = 1,     /**< um inteiro */
+   DOUBLE = 2,  /**< um double */
+   CHAR = 4,    /**< um char */
+   STRING = 8,  /**< uma string */
+   ARRAY = 16, /**< um array */
+   BLOCK = 32 /**< um bloco */
 } TYPE;
 
-/** struct que carrega a forma dos elementos da stack */
+#define INTEGER  (INT | CHAR)
+#define NUMBER   (INTEGER | DOUBLE)
+
+typedef struct {
+   int size;
+   struct data *elems;
+} ARRAYS;
+
+/** \struct carrega a forma dos elementos da stack */
 
 typedef struct data {
    TYPE type; /**< o tipo do elemento */
    /** carrega diferentes tipos de elementos */
    union {
-      int INT; /**< o elemento quando int */
+      long INT; /**< o elemento quando int */
       double DOUBLE; /**< o elemento quando double */
       char CHAR; /**< o elemento quando char */
       char *STRING; /**< o elemento quando string */
+      char *BLOCK; /**< o elemento quando bloco */
+      ARRAYS array; /**< o elemento quando array */
    };
 
 } DATA;
 
-/** \brief a stack do programa */
+/** \struct a stack do programa */
 
 typedef struct stack {
    DATA *stack; /**< os elementos da stack */
-   DATA *variables; /**< as variáveis */
+   DATA variables[26]; /**< as variáveis */
    int size; /**< o tamanho da stack */
    int pos; /**< a posição em que se encontra o elemento do topo da stack */
 } STACK;
+
+int has_type(DATA elem, int mask);
 
 /** \brief Inicializa os valores da stack */ 
 STACK* create_stack ();
@@ -52,19 +65,20 @@ DATA top (STACK *s);
 void push_unary_operation(STACK *s, double val);
 
 /** \brief da push a um elemento resultante de uma operação binária, consoante o seu tipo */ 
-void push_binary_operation(STACK *s, double val);
+void push_binary_operation(STACK *s, long double val);
 
 /** \brief devolve o elemento do topo da stack consoante o seu tipo */ 
-double pop_operand(STACK *s);
+long double pop_operand(STACK *s);
 
 /** \brief declaração das funções push e pop dos tipos STRING, CHAR, DOUBLE, INT*/
 #define STACK_OPERATION_PROTO(_type, _name) \
    void push_##_name(STACK *s, _type);      \
    _type pop_##_name(STACK *s);             
 
-STACK_OPERATION_PROTO(int, INT)
+STACK_OPERATION_PROTO(long, INT)
 STACK_OPERATION_PROTO(double, DOUBLE)
 STACK_OPERATION_PROTO(char, CHAR)
 STACK_OPERATION_PROTO(char*, STRING)
+STACK_OPERATION_PROTO(char*, BLOCK)
 
 #endif
