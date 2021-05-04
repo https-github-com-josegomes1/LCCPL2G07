7d1756343977ac1e0e8fa5d4d506c13fc04c8543
EduDCS <77997232+EduardoDCS2002@users.noreply.github.com>
2021-05-04 11:18:43 +0100

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -3,18 +3,42 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#include "parser.h"
+#include "stack.h"
 
-/** \brief Define todos os tokens dos operadores */
-char** get_operators_token();
+/** \brief determina se o token corresponde a uma operação aritmética */
+int Is_arithmetic_operation(STACK *s, char *token);
 
-/** \brief Define todas as funções dos operadores */
-parse_operators_function* get_operators_function();
+/** \brief determina se o token corresponde a uma operação lógica */
+int Is_logic_operation (STACK *s, char *token);
 
-/** \brief Define o estado das condições dos operandos*/
-int* get_operands_state(char *token);
+/** \brief determina se o token corresponde a uma operação envolvendo array */
+int Is_array_operation(STACK *s, char *token);
 
-/** \brief Define todas as funções dos operandos */
-parse_operands_function* get_operands_function();
+/** \brief determina se o token corresponde a uma operação envolvendo string */
+int Is_string_operation(STACK *s, char *token);
+
+/** \brief determina se o token corresponde a uma operação de stack */
+int Is_stack_operation(STACK *s, char *token);
+
+/** \brief determina se o token corresponde a uma operação de input/output */
+int Is_input_output_operation(STACK *s, char *token);
+
+/** \brief determina se o token corresponde a uma operação envolvendo bloco */
+int Is_block_operation(STACK *s, char *token);
+
+/** \brief determina se o token é um número */
+int Is_token_number(char *token);
+
+/** \brief determina se o token é um array */
+int Is_token_array(char *token);
+
+/** \brief determina se o token é uma string */
+int Is_token_string(char * token);
+
+/** \brief determina se o token é uma variável */
+int Is_token_variable(char * token);
+
+/** \brief determina se o token é um bloco */
+int Is_token_block(char * token);
 
 #endif
